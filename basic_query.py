45d8e13e910f4e5c1de4236de4c70040eadaccce
Ames75 <orc.learner@gmail.com>
2020-06-14 11:12:51 -0400

    initial basic query

---
+++ basic_query.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import dns.message
+import dns.rdataclass
+import dns.rdatatype
+import dns.query
+import sys
+
+def main(argv):
+    if len(argv) > 1:
+        print("total arguments is %d" % len(argv));
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
