8aa84b0948cc150e63dc6a24112ab11a4c7dbca4
Guojinhe-GH <guojinhe-gh@guojinhe-mb1.tencent.com>
2020-07-06 19:49:33 -0400

    use argparser

--- basic_query.py
+++ basic_query.py
@@ -5,9 +5,16 @@ import dns.rdataclass
 import dns.rdatatype
 import dns.query
 import sys
+import argparse
 
 def usage():
-    print("usage: basic_query.py <domain name> [name server ip]")
+    print("usage: basic_query.py <domain name> [name server ip] [options]")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain_name", help="Domain name to query about",type=str)
+    parser.add_argument("--dns_server", help="DNS Server IP to query against")
+    return parser.parse_args()
 
 def main(argv):
     if len(argv) > 1:
@@ -15,8 +22,9 @@ def main(argv):
     else:
         usage()
         exit()
-    ns_server = argv[2] if len(argv) > 2 else '8.8.8.8'   
-    qname = dns.name.from_text(argv[1])    
+    args = parse_arguments()
+    ns_server = args.dns_server or '8.8.8.8'   
+    qname = dns.name.from_text(args.domain_name)    
     qmsg = dns.message.make_query(qname, dns.rdatatype.A)
     #qmsg.use_edns(options=[dns.edns.ECSOption('1.2.3.4',26)])
     reply = dns.query.udp(qmsg,ns_server) 
