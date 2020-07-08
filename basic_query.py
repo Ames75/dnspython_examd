7be13020cd40477f3b9beb53d86a48e55c113e25
Guojinhe-GH <guojinhe-gh@guojinhe-mb1.tencent.com>
2020-07-08 16:34:44 -0400

    add ECS query

--- basic_query.py
+++ basic_query.py
@@ -6,6 +6,7 @@ import dns.rdatatype
 import dns.query
 import sys
 import argparse
+import ipaddress
 
 def usage():
     print("usage: basic_query.py <domain name> [name server ip] [options]")
@@ -14,7 +15,27 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("domain_name", help="Domain name to query about",type=str)
     parser.add_argument("--dns_server", help="DNS Server IP to query against")
+    parser.add_argument("--subnet", help="EDNS-Client-Subnet")
     return parser.parse_args()
+
+# return true if ECS info is right and added to query
+def add_ECS_options(subnet_info, qmsg):
+    addr,scope = subnet_info.split('/')
+    try:
+        ip = ipaddress.ip_address(addr)
+        if isinstance(ip,ipaddress.IPv4Address):
+            if not (1<= int(scope) <= 32 ):
+               print("net scope %d not right" % int(scope))
+            else:
+               qmsg.use_edns(options=[dns.edns.ECSOption(addr,int(scope))])
+               return True
+        else:
+            print("no support for V6 addr yet")     
+        return False
+    except ValueError:
+        print("invalid ip %s" % addr)
+        return False
+        
 
 def main(argv):
     if len(argv) > 1:
@@ -23,10 +44,12 @@ def main(argv):
         usage()
         exit()
     args = parse_arguments()
-    ns_server = args.dns_server or '8.8.8.8'   
+    ns_server = args.dns_server or '119.29.29.29'  
+    subnet_info = args.subnet
     qname = dns.name.from_text(args.domain_name)    
     qmsg = dns.message.make_query(qname, dns.rdatatype.A)
-    #qmsg.use_edns(options=[dns.edns.ECSOption('1.2.3.4',26)])
+    if args.subnet and add_ECS_options(args.subnet,qmsg):
+        print("doing ECS query")
     reply = dns.query.udp(qmsg,ns_server) 
     print(reply)
 
