02402e5b90f9b21b6e9f765adf7faa44613a9a65
Your Name <you@example.com>
2020-09-10 16:18:34 -0400

    get query in generate_test

--- dns_class_defs.py
+++ dns_class_defs.py
@@ -3,12 +3,13 @@ import dns.rcode
 import ipaddress
 
 class Reply(dict):
-  def __init__(self,ips):
-    # flags should be in the order of AA,TC,RD,RA  
-    dict.__init__(self, flags=[True, False, True, True],
-                  ips=ips,qname="",cname="",
-                  ttl=0, rtype = dns.rcode.NOERROR)
-    self.flags = [True, False, True, True]
+  def __init__(self,ips,flag_list,rtype=dns.rcode.NOERROR):
+    # flags should be in the order of AA,TC,RA, all boolean  
+    dict.__init__(self, 
+                  flag_list,
+                  ips,qname="",cname="",
+                  ttl=0, rtype=dns.rcode.NOERROR)
+    self.flags = flag_list
     self.ips = ips
     self.qname = ""
     self.cname = ""
--- generate_tests.py
+++ generate_tests.py
@@ -9,6 +9,45 @@ import dns.query
 import sys
 import argparse
 import ipaddress
+import re
+import dns_class_defs
+
+""" TestGenerator class """
+class TestGenerator():
+    def __init__(self, nsip):
+        self.nsip = nsip
+    """ query_name returns single Reply oject out of qname """    
+    def query_name(self, name):
+        qname = dns.name.from_text(name)    
+        qmsg = dns.message.make_query(qname, dns.rdatatype.A)
+        # we do not want recursion as we are only testing authority
+        qmsg.flags = 0
+        reply = dns.query.udp(qmsg,self.nsip)
+        if not (reply.flags & dns.flags.QR):
+            print("query ", qname, " is not getting reply as QR bit is set") 
+            return None
+        flag_list = [bool(reply.flags & dns.flags.AA),  
+                     bool(reply.flags & dns.flags.TC),
+                     bool(reply.flags & dns.flags.RA)]
+        a_records_set = reply.get_rrset(dns.message.ANSWER, 
+                                       qname, dns.rdataclass.IN, 
+                                       dns.rdatatype.A)
+        if a_records_set is None:
+            print("could not find A answer for ",name)
+            return None 
+        ips = []
+        for record in a_records_set:
+            ips.append(record.address)
+        #print(type(a_records_set))
+        #print(a_records_set.to_text())
+        reply_obj = dns_class_defs.Reply(ips,flag_list)
+        #print(flag_list)
+        #print(reply.to_text())
+        return reply_obj
+    def query_all_names(self, qname_list):
+        print("number of names are ", len(qname_list))
+        for name in qname_list:
+            self.query_name(name)
 
 def usage():
     print("usage: generat_tests.py <nameserver ip> [query names or input file] [src ip or subnet]")
@@ -18,11 +57,21 @@ def parse_arguments():
     group = parser.add_mutually_exclusive_group()
     parser.add_argument("nsip", help="name server that generates expected test results")
     group.add_argument("-n", "--names", nargs='*', help="query name to use, you can have multiple names")
-    group.add_argument("-i", "--input", nargs=1, help="input file of query names, cannot be used with -n")
+    group.add_argument("-i", "--input_file", help="input file of query names, cannot be used with -n", type=str)
     parser.add_argument("-s", "--src", nargs='?', help="source ip to use, if it is in subnet, then its ECS")
     return parser.parse_args()
 
 def getQnamesFromFile(input_file):
+    try:
+        with open(input_file) as fp:
+           content = fp.read() 
+           return list(filter(None,re.split("[;,\s]",content)))                
+    except:
+        print("fail to open file ", input_file)
+        print(sys.exc_info()[0])    
+        exit()
+    finally:
+        fp.close() 
     return None
 
 def main(argv):
@@ -30,12 +79,12 @@ def main(argv):
         usage()
         exit()
     args = parse_arguments()
-    print(args.nsip)
-    print(type(args.names))
-    from_file = args.input is not None
-    query_names = args.names if not from_file else getQnamesFromFile(args.input) 
-    for value in query_names:
-        print(value)    
+    print("send querie to ", args.nsip)
+    from_file = args.input_file is not None
+    #print(type(args.input_file))
+    query_names = args.names if not from_file else getQnamesFromFile(args.input_file) 
+    test1 = TestGenerator(args.nsip)
+    test1.query_all_names(query_names) 
 
 if __name__ == "__main__":
     main(sys.argv)
